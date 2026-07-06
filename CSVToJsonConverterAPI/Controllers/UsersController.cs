cd8107e51f9bc630b2c8925c3869bad02d5ddcd3
Stefan Abramsson <stab2300@student.miun.se>
2026-07-06 11:55:03 +0200

    Created a UsersController to handle response from user. If the user has added a value to the parameter a limiter is used. If the parameter has a negative value it returns 400 BadRequest.

---
+++ CSVToJsonConverterAPI/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSVToJsonConverterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet()]
+        public async Task<IActionResult> GetUsers(int limit)
+        {
+            
+            if (int.IsNegative(limit))
+                return BadRequest();
+
+            try
+            {
+
+                // TODO update the controller to return the result.
+                return Ok();
+            }
+            catch (Exception)
+            {
+                // TODO if file not found exception return 500
+                return StatusCode(StatusCodes.Status500InternalServerError);
+                throw;
+            }
+        }
+    }
+}
